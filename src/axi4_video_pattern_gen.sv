fad05de3c9672f7f881748740299cdb44b14c712
Leonid Ivanov <l.ivanov@metrotek.ru>
2019-04-21 14:29:23 +0300

    timing fixes

--- src/axi4_video_pattern_gen.sv
+++ src/axi4_video_pattern_gen.sv
@@ -13,14 +13,14 @@ localparam int Y_RES         = Y_ACTIVE + Y_BLANKING;
 localparam int X_RES         = X_ACTIVE + X_BLANKING;
 localparam int PX_CNT_WIDTH  = $clog2( X_RES );
 localparam int LN_CNT_WIDTH  = $clog2( Y_RES );
-localparam int LANE_1_BORDER = ( Y_ACTIVE / 8 ) * 1;
-localparam int LANE_2_BORDER = ( Y_ACTIVE / 8 ) * 2;
-localparam int LANE_3_BORDER = ( Y_ACTIVE / 8 ) * 3;
-localparam int LANE_4_BORDER = ( Y_ACTIVE / 8 ) * 4;
-localparam int LANE_5_BORDER = ( Y_ACTIVE / 8 ) * 5;
-localparam int LANE_6_BORDER = ( Y_ACTIVE / 8 ) * 6;
-localparam int LANE_7_BORDER = ( Y_ACTIVE / 8 ) * 7;
-localparam int LANE_8_BORDER = ( Y_ACTIVE / 8 ) * 8;
+localparam int LANE_1_BORDER = ( X_ACTIVE / 8 ) * 1;
+localparam int LANE_2_BORDER = ( X_ACTIVE / 8 ) * 2;
+localparam int LANE_3_BORDER = ( X_ACTIVE / 8 ) * 3;
+localparam int LANE_4_BORDER = ( X_ACTIVE / 8 ) * 4;
+localparam int LANE_5_BORDER = ( X_ACTIVE / 8 ) * 5;
+localparam int LANE_6_BORDER = ( X_ACTIVE / 8 ) * 6;
+localparam int LANE_7_BORDER = ( X_ACTIVE / 8 ) * 7;
+localparam int LANE_8_BORDER = ( X_ACTIVE / 8 ) * 8;
 
 logic [PX_CNT_WIDTH - 1 : 0] px_cnt;
 logic [LN_CNT_WIDTH - 1 : 0] ln_cnt;
@@ -48,7 +48,7 @@ always_ff @( posedge clk_i, posedge rst_i )
   if( rst_i )
     video_o.tvalid <= 1'b0;
   else
-    if( ( ln_cnt < Y_RES ) && ( px_cnt < X_RES ) )
+    if( ( ln_cnt < Y_ACTIVE ) && ( px_cnt < X_ACTIVE ) )
       video_o.tvalid <= 1'b1;
     else
       video_o.tvalid <= 1'b0;
@@ -64,7 +64,7 @@ always_ff @( posedge clk_i, posedge rst_i )
         video_o.tdata <= 32'h000003ff;
       else
         if( px_cnt < LANE_3_BORDER )
-          video_o.tdata <= 32'h000ffc00;
+          video_o.tdata <= 32'h00ffc00;
         else
           if( px_cnt < LANE_4_BORDER )
             video_o.tdata <= 32'h000fffff;
@@ -84,7 +84,7 @@ always_ff @( posedge clk_i, posedge rst_i )
   if( rst_i )
     video_o.tlast <= 1'b0;
   else
-    if( px_cnt == ( X_RES - 1 ) )
+    if( px_cnt == ( X_ACTIVE - 1 ) )
       video_o.tlast <= 1'b1;
     else
       video_o.tlast <= 1'b0;
@@ -93,8 +93,8 @@ always_ff @( posedge clk_i, posedge rst_i )
   if( rst_i )
     video_o.tuser <= 1'b0;
   else
-    if( ( px_cnt == ( X_RES - 1 ) ) &&
-        ( ln_cnt == ( Y_RES - 1 ) ) )
+    if( ( px_cnt == '0 ) &&
+        ( ln_cnt == '0 ) )
       video_o.tuser <= 1'b1;
     else
       video_o.tuser <= 1'b0;
